ffaefade1345c626cb6c1dac593edd5fcb409646
Luc <info@feron.it>
2021-12-18 17:58:02 +0100

    d18 start

---
+++ Day18.cs
@@ -0,0 +1,21 @@
+using System.Text;
+
+namespace Aoc2021
+{
+    static class Day18
+    {
+
+        //static
+        static string input = @"";
+        static string input2 = @"";
+        
+        public static void Execute()
+        {
+            string inp = input2;
+            var lines = inp.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            
+        }
+
+    }
+    public static class snailfish 
+}
